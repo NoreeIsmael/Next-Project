5f5ce69a1d00c3fd3de970e4329d702310907b06
LobaDK <gta5.r6s.bf4@gmail.com>
2024-10-14 14:31:22 +0200

    Updated config path to use new app-specific config file. Improved handling and verbosity for migration. Added logger instance specific to database migrations

--- backend/lib/sql/__init__.py
+++ backend/lib/sql/__init__.py
@@ -1,4 +1,4 @@
-from logging import Logger, DEBUG, CRITICAL
+from logging import Logger, DEBUG, WARNING
 
 from backend.lib._logger import LogHelper
 
@@ -6,6 +6,14 @@ db_logger: Logger = LogHelper.create_logger(
     logger_name="sqlalchemy",
     log_file="backend/logs/sql.log",
     file_log_level=DEBUG,
-    stream_log_level=CRITICAL,
+    stream_log_level=WARNING,
+    ignore_existing=True,
+)
+
+database_migration_logger: Logger = LogHelper.create_logger(
+    logger_name="alembic",
+    log_file="backend/logs/alembic.log",
+    file_log_level=DEBUG,
+    stream_log_level=WARNING,
     ignore_existing=True,
 )
